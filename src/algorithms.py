7daa8882dd1d479da3110d9de29b8d93a84078f3
Liam Ek <Liam.a.ek05@gmail.com>
2024-06-11 01:15:58 +0200

    Changed function order

--- src/algorithms.py
+++ src/algorithms.py
@@ -18,38 +18,6 @@ def bubble_sort(data, draw_data, speed):
                 time.sleep(speed)
 
 
-def partition(data, draw_data, low, high, speed):
-    """Quick sort: Find partition position"""
-
-    pivot = data[high]
-    i = low - 1
-
-    draw_data(data, get_color(len(data), low, high, i, i))
-
-    time.sleep(speed)
-
-    for j in range(low, high):
-
-        if data[j] <= pivot:
-            draw_data(data, get_color(len(data), low, high, i, j, True))
-
-            i += 1
-
-            data[i], data[j] = data[j], data[i]
-
-        draw_data(data, get_color(len(data), low, high, i, j))
-
-        time.sleep(speed)
-
-    draw_data(data, get_color(len(data), low, high, i, low, True))
-
-    time.sleep(speed)
-
-    data[i + 1], data[high] = data[high], data[i + 1]
-
-    return i + 1
-
-
 def quick_sort(data, draw_data, low, high, speed):
     """Quick sort algorithm"""
 
@@ -89,3 +57,38 @@ def get_color(data_len, low, high, border, cur_i, is_swapping=False):
                 color_array[i] = 'Green'
 
     return color_array
+
+
+def partition(data, draw_data, low, high, speed):
+    """Quick sort: Find partition position"""
+
+    pivot = data[high]
+    i = low - 1
+
+    draw_data(data, get_color(len(data), low, high, i, i))
+
+    time.sleep(speed)
+
+    for j in range(low, high):
+
+        if data[j] <= pivot:
+            draw_data(data, get_color(len(data), low, high, i, j, True))
+
+            i += 1
+
+            data[i], data[j] = data[j], data[i]
+
+        draw_data(data, get_color(len(data), low, high, i, j))
+
+        time.sleep(speed)
+
+    draw_data(data, get_color(len(data), low, high, i, low, True))
+
+    time.sleep(speed)
+
+    data[i + 1], data[high] = data[high], data[i + 1]
+
+    return i + 1
+
+
+
