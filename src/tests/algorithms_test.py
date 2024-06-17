a3c7ebc2209889578c056e080c73c462dc60db07
HarbourHeading <Liam.a.ek05@gmail.com>
2024-06-17 16:18:47 +0200

    Added unittest for `merge_sort`

--- src/tests/algorithms_test.py
+++ src/tests/algorithms_test.py
@@ -1,5 +1,8 @@
+"""Module to unittest algorithms"""
+
 import unittest
 from unittest.mock import Mock
+
 import src.algorithms as algorithms
 
 
@@ -11,7 +14,7 @@ class TestMain(unittest.TestCase):
 
         data = [9, 2, 7, 5, -1, 5, 23, 0, 61, 8, 9, 11, -22]
 
-        algorithms.bubble_sort(data, draw_data_mock, 0)
+        algorithms.bubble_sort_algorithm(data, draw_data_mock, 0)
 
         self.assertEqual([-22, -1, 0, 2, 5, 5, 7, 8, 9, 9, 11, 23, 61], data)
 
@@ -21,7 +24,18 @@ class TestMain(unittest.TestCase):
 
         data = [9, 2, 7, 5, -1, 5, 23, 0, 61, 8, 9, 11, -22]
 
-        algorithms.quick_sort(data, draw_data_mock, 0, len(data) - 1, 0)
+        algorithms.quick_sort_algorithm(data, draw_data_mock, 0, len(data) - 1, 0)
+
+        self.assertEqual([-22, -1, 0, 2, 5, 5, 7, 8, 9, 9, 11, 23, 61], data)
+
+    def test_merge_sort(self):
+        """Merge sort: test correct sorting"""
+
+        draw_data_mock = Mock()
+
+        data = [9, 2, 7, 5, -1, 5, 23, 0, 61, 8, 9, 11, -22]
+
+        algorithms.merge_sort_algorithm(data, draw_data_mock, 0, len(data) - 1, 0)
 
         self.assertEqual([-22, -1, 0, 2, 5, 5, 7, 8, 9, 9, 11, 23, 61], data)
 
